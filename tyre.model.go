7f6dec0b59e8266e87dd3d95a2c464a490000711
ramiresGit <ramiresgit@gmail.com>
2023-10-30 22:58:57 +0300

    to one file

--- tyre.model.go
+++ tyre.model.go
@@ -34,12 +34,23 @@ type TyreModel struct {
 	MarginPrice    int      `bun:"marginPrice" json:"marginPrice"`
 }
 type TyreRequestModel struct {
-	Meta  Meta        `json:"meta"`
+	Meta  meta        `json:"meta"`
 	Shops []string    `json:"shops"`
 	Tyres []TyreModel `json:"tyre"`
 }
 
-type Meta struct {
+type meta struct {
 	Created   string `json:"created"`
 	Generator string `json:"generator"`
+}
+
+type SearchModel struct {
+	Brand    string  `json:"brand"`
+	Season   string  `json:"season"`
+	Diameter string  `json:"diameter"`
+	Width    float32 `json:"width"`
+	Profile  float32 `json:"profile"`
+	RunFlat  bool    `json:"runflat"`
+	Seal     bool    `json:"seal"`
+	Acoustic bool    `json:"acoustic"`
 }
